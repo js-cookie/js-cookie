6818b83dff89e9cac9e95acd9d78bf0179777e3d
carhartl <klaus.hartl@stilbuero.de>
2010-01-08 23:17:31 +0100

    introduced "raw" option as to whether encode-/decodeURIComponent cookie value or not... (there was an issues when encoded value is read server-side)

--- jquery.cookie.js
+++ jquery.cookie.js
@@ -1,7 +1,7 @@
 /*jslint browser: true */ /*global jQuery: true */
 
 /**
- * Cookie plugin
+ * jQuery Cookie plugin
  *
  * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
  * Dual licensed under the MIT and GPL licenses:
@@ -60,7 +60,7 @@
 jQuery.cookie = function (key, value, options) {
 
     // key and value given, set cookie...
-    if (arguments.length > 1) {
+    if (arguments.length > 1 && (value === null || typeof value !== "object")) {
         options = jQuery.extend({}, options);
 
         if (value === null) {
@@ -72,7 +72,9 @@ jQuery.cookie = function (key, value, options) {
             t.setDate(t.getDate() + days);
         }
 
-        return (document.cookie = [key, '=', encodeURIComponent(String(value)),
+        return (document.cookie = [
+            encodeURIComponent(key), '=',
+            options.raw ? String(value) : encodeURIComponent(String(value)),
             options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
             options.path ? '; path=' + options.path : '',
             options.domain ? '; domain=' + options.domain : '',
@@ -80,7 +82,8 @@ jQuery.cookie = function (key, value, options) {
         ].join(''));
     }
 
-    // get cookie...
-    var result;
-    return (result = new RegExp('(?:^|; )' + key + '=([^;]*)').exec(document.cookie)) ? decodeURIComponent(result[1]) : null;
+    // key and possibly options given, get cookie...
+    options = value || {};
+    var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
+    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
 };
--- test/test.js
+++ test/test.js
@@ -1,29 +1,39 @@
 var before = {
     setup: function () {
         document.cookie = 'c=; expires=' + new Date(0).toUTCString();
+        document.cookie = encodeURIComponent(' foo ') + '=; expires=' + new Date(0).toUTCString();
+        document.cookie = 'raw=; expires=' + new Date(0).toUTCString();
     }
 };
 
 module('read', before);
 
 test('Read', function () {
-    expect(3);
+    expect(5);
 
     equals($.cookie('c'), null);
     document.cookie = 'c=v';
     equals($.cookie('c'), 'v', 'Read cookie value');
+
+    document.cookie = encodeURIComponent(' foo ') + '=' + encodeURIComponent(' bar ');
+    equals($.cookie(' foo '), ' bar ', 'Decode key/value');
+
+    document.cookie = 'raw=%20whatever';
+    equals($.cookie('raw', { raw: true }), '%20whatever', 'Do not decode with raw set true');
+
     equals($.cookie('whatever'), null, 'Give null if cookie does not exist');
 });
 
 module('Write', before);
 
 test('Write', function () {
-    expect(3);
+    expect(4);
 
     equals($.cookie('c'), null);
     var cookie = $.cookie('c', 'v');
     equals(document.cookie, 'c=v', 'Write cookie value');
-    equals(cookie, 'c=v', 'Return cookie');
+    equals(cookie, 'c=v', 'Return cookie string');
+    equals($.cookie('raw', ' whatever', { raw: true }).split('=')[1], ' whatever', 'Do not encode with raw set true');
 });
 
 module('Delete', before);
