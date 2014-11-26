f12dce392ae87bcc50ee76d57ab8e59f60f522d6
Konstantinos Vaggelakos <kozze89@gmail.com>
2014-11-26 01:56:27 +0100

    Commonjs export fix and jshint updates

--- src/jquery.cookie.js
+++ src/jquery.cookie.js
@@ -11,7 +11,7 @@
 		define(['jquery'], factory);
 	} else if (typeof exports === 'object') {
 		// CommonJS
-		factory(require('jquery'));
+		module.exports = factory(require('jquery'));
 	} else {
 		// Browser globals
 		factory(jQuery);
