8b28ab2091e966b78f8d901df02e317e00bcde6b
Klaus Hartl <klaus.hartl@gmail.com>
2015-03-18 21:17:11 +0100

    Improving comments somewhat

--- src/jquery.cookie.js
+++ src/jquery.cookie.js
@@ -7,10 +7,10 @@
  */
 (function (factory) {
 	if (typeof define === 'function' && define.amd) {
-		// AMD
+		// AMD (Register as an anonymous module)
 		define(['jquery'], factory);
 	} else if (typeof exports === 'object') {
-		// CommonJS
+		// Node/CommonJS
 		module.exports = factory(require('jquery'));
 	} else {
 		// Browser globals
