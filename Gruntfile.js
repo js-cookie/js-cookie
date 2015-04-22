ec03a6d394b3e9372513f7d1f5c8a5464077d894
Fagner Brack <github@fagnermartins.com>
2015-04-22 17:45:34 -0300

    Update Sauce browsers
    * Test in Android emulator, ignore other devices
    * Test in iPhone and iPad emulators
    * Test in Windows 7 for IE 9, 10, 11 and the latest version of Opera
    * Test in Windows XP for IE 6, 7 and 8
    * Test for all latest browser versions in Sauce for OS X Yosemite
    (ignore older OS)
    * Test for all latest browser versions in Sauce for Linux, except Lynx

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,89 +83,92 @@ module.exports = function (grunt) {
 					urls: ['http://127.0.0.1:9999'],
 					testname: 'Sauce Test for js-cookie',
 					build: process.env.TRAVIS_JOB_ID,
-					pollInterval: 5000,
-					browsers: [
-						// iOS
-						{
-							browserName: 'iphone',
-							platform: 'OS X 10.9',
-							version: '7.1'
-						},
-						{
-							browserName: 'ipad',
-							platform: 'OS X 10.9',
-							version: '7.1'
-						},
-						// Android
-						{
-							browserName: 'android',
-							platform: 'Linux',
-							version: '4.3'
-						},
-						// OS X
-						{
-							browserName: 'safari',
-							platform: 'OS X 10.9',
-							version: '7'
-						},
-						{
-							browserName: 'safari',
-							platform: 'OS X 10.8',
-							version: '6'
-						},
-						{
-							browserName: 'firefox',
-							platform: 'OS X 10.9',
-							version: '28'
-						},
-						// Windows
-						{
-							browserName: 'internet explorer',
-							platform: 'Windows 8.1',
-							version: '11'
-						},
-						{
-							browserName: 'internet explorer',
-							platform: 'Windows 8',
-							version: '10'
-						},
-						{
-							browserName: 'internet explorer',
-							platform: 'Windows 7',
-							version: '11'
-						},
-						{
-							browserName: 'internet explorer',
-							platform: 'Windows 7',
-							version: '10'
-						},
-						{
-							browserName: 'internet explorer',
-							platform: 'Windows 7',
-							version: '9'
-						},
-						{
-							browserName: 'internet explorer',
-							platform: 'Windows 7',
-							version: '8'
-						},
-						{
-							browserName: 'firefox',
-							platform: 'Windows 7',
-							version: '29'
-						},
-						{
-							browserName: 'chrome',
-							platform: 'Windows 7',
-							version: '34'
-						},
-						// Linux
-						{
-							browserName: 'firefox',
-							platform: 'Linux',
-							version: '29'
+					pollInterval: 10000,
+					statusCheckAttempts: 90,
+					throttled: 3,
+					browsers: (function() {
+						var browsers = {
+							'iOS': [{
+								browserName: 'iphone',
+								platform: 'OS X 10.10',
+								version: '8.2',
+								deviceName: 'iPhone Simulator'
+							}, {
+								browserName: 'iphone',
+								platform: 'OS X 10.10',
+								version: '8.2',
+								deviceName: 'iPad Simulator'
+							}],
+							'android': [{
+								browserName: 'android',
+								platform: 'Linux',
+								version: '5.1',
+								deviceName: 'Android Emulator'
+							}],
+							'mac': [{
+								browserName: 'safari',
+								platform: 'OS X 10.10',
+								version: '8.0'
+							}, {
+								browserName: 'firefox',
+								platform: 'OS X 10.10',
+								version: '36.0'
+							}, {
+								browserName: 'chrome',
+								platform: 'OS X 10.10',
+								versiono: '41.0'
+							}],
+							'windows7': [{
+								browserName: 'internet explorer',
+								platform: 'Windows 7',
+								version: '11.0'
+							}, {
+								browserName: 'internet explorer',
+								platform: 'Windows 7',
+								version: '10.0'
+							}, {
+								browserName: 'internet explorer',
+								platform: 'Windows 7',
+								version: '9.0'
+							}, {
+								browserName: 'opera',
+								platform: 'Windows 7',
+								version: '12.12'
+							}],
+							'windowsXP': [{
+								browserName: 'internet explorer',
+								platform: 'Windows XP',
+								version: '8.0'
+							}, {
+								browserName: 'internet explorer',
+								platform: 'Windows XP',
+								version: '7.0'
+							}, {
+								browserName: 'internet explorer',
+								platform: 'Windows XP',
+								version: '6.0'
+							}],
+							'linux': [{
+								browserName: 'opera',
+								platform: 'Linux',
+								version: '12.15'
+							}, {
+								browserName: 'firefox',
+								platform: 'Linux',
+								version: '37.0'
+							}, {
+								browserName: 'chrome',
+								platform: 'Linux',
+								version: '41.0'
+							}]
+						};
+
+						var matrix = [];
+						for ( var os in browsers ) {
+							matrix = matrix.concat(browsers[os]);
 						}
-					]
+						return matrix;
+					}())
 				}
 			}
 		}
