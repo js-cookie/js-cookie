514589cd4bfbd5fc40edf4582bd056ebc795d485
Fagner Brack <github@fagnermartins.com>
2016-07-31 17:37:13 +1000

    Remove QUnit.expect from amd test
    QUnit v2 have removed this API. This prepares for when we decide to upgrade.

--- test/amd.js
+++ test/amd.js
@@ -3,7 +3,7 @@ require(['qunit'], function (QUnit) {
 
 	QUnit.start();
 	QUnit.test('module loading', function (assert) {
-		QUnit.expect(1);
+		assert.expect(1);
 		var done = assert.async();
 		require(['/src/js.cookie.js'], function (Cookies) {
 			assert.ok(!!Cookies.get, 'should load the api');
