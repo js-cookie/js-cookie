e7ef7d750340e76414fe741a3994e7ce5db532d5
Fagner Brack <github@fagnermartins.com>
2017-10-28 14:58:31 +1100

    Add again SauceLabs support for IE 8 under Windows 7 umbrella
    Due to d3495561e6 we can't yet test on IE 6 and IE 7

--- Gruntfile.js
+++ Gruntfile.js
@@ -188,6 +188,10 @@ module.exports = function (grunt) {
 								browserName: 'internet explorer',
 								platform: 'Windows 7',
 								version: '9.0'
+							}, {
+								browserName: 'internet explorer',
+								platform: 'Windows 7',
+								version: '8.0'
 							}],
 							'linux': [{
 								browserName: 'firefox',
