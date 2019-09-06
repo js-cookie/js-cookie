e3a09f3f72ea9ea83e4866e50a81dced16f1eb08
Klaus Hartl <klaus.hartl@gmail.com>
2019-09-06 11:23:03 +0200

    Port uglify grunt task to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,44 @@
+import { terser } from "rollup-plugin-terser";
+import license from "rollup-plugin-license";
+import pkg from "./package.json";
+
 export default {
   input: "src/js.cookie.mjs",
   output: [
     // config for <script type="module">
     {
       dir: "build",
-      entryFileNames: "[name].mjs",
+      entryFileNames: "[name].min.mjs",
+      format: "esm"
+    },
+    {
+      dir: "build",
+      entryFileNames: `[name]-${pkg.version}.min.mjs`,
       format: "esm"
     },
     // config for <script nomodule>
     {
       dir: "build",
       name: "Cookies",
-      entryFileNames: "[name].js",
+      entryFileNames: "[name].min.js",
+      format: "umd"
+    },
+    {
+      dir: "build",
+      name: "Cookies",
+      entryFileNames: `[name]-${pkg.version}.min.js`,
       format: "umd"
     }
+  ],
+  // rollup-plugin-uglify only works with babel...
+  plugins: [
+    terser(),
+    license({
+      banner: {
+        content:
+          "/*! <%= pkg.name %> v<%= pkg.version %> | <%= pkg.license %> */",
+        commentStyle: "none"
+      }
+    })
   ]
 };
