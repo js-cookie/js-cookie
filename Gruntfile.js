df76a881cd02848924e8fc73d75730c7f264f955
Klaus Hartl <klaus.hartl@gmail.com>
2019-09-26 12:19:06 +0200

    Modernize Gruntfile.js file
    Also trying to improve code climate rating...

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,101 +1,99 @@
+function encodingMiddleware (request, response, next) {
+  const URL = require('url').URL
+  var url = new URL(request.url, 'http://localhost')
+
+  if (url.pathname !== '/encoding') {
+    next()
+    return
+  }
+
+  var cookieName = url.searchParams.get('name')
+  var cookieValue = url.searchParams.get('value')
+
+  response.setHeader('content-type', 'application/json')
+  response.end(
+    JSON.stringify({
+      name: cookieName,
+      value: cookieValue
+    })
+  )
+}
+
+const config = {
+  qunit: {
+    all: {
+      options: {
+        urls: [
+          'http://127.0.0.1:9998/',
+          'http://127.0.0.1:9998/module.html',
+          'http://127.0.0.1:9998/encoding.html?integration_baseurl=http://127.0.0.1:9998/'
+        ]
+      }
+    }
+  },
+  nodeunit: {
+    all: 'test/node.js'
+  },
+  watch: {
+    options: {
+      livereload: true
+    },
+    files: ['src/**/*.mjs', 'test/**/*.js'],
+    tasks: 'default'
+  },
+  compare_size: {
+    files: [
+      'dist/js.cookie.min.mjs',
+      'dist/js.cookie.min.js',
+      'src/js.cookie.mjs'
+    ],
+    options: {
+      compress: {
+        gz: fileContents => require('gzip-js').zip(fileContents, {}).length
+      }
+    }
+  },
+  connect: {
+    'build-qunit': {
+      options: {
+        port: 9998,
+        base: ['.', 'test'],
+        middleware: function (connect, options, middlewares) {
+          middlewares.unshift(encodingMiddleware)
+          return middlewares
+        }
+      }
+    },
+    tests: {
+      options: {
+        port: 10000,
+        base: ['.', 'test'],
+        open: 'http://127.0.0.1:10000',
+        keepalive: true,
+        livereload: true,
+        middleware: function (connect, options, middlewares) {
+          middlewares.unshift(encodingMiddleware)
+          return middlewares
+        }
+      }
+    }
+  },
+  exec: {
+    rollup: './node_modules/.bin/rollup -c',
+    lint: './node_modules/.bin/standard',
+    format:
+      './node_modules/.bin/prettier -l --write --single-quote --no-semi "**/*.{html,js,json,md,mjs}" && ./node_modules/.bin/eslint "**/*.{html,md}" --fix && ./node_modules/.bin/standard --fix',
+    'browserstack-runner': 'node_modules/.bin/browserstack-runner --verbose'
+  }
+}
+
 module.exports = function (grunt) {
-  function encodingMiddleware (request, response, next) {
-    const URL = require('url').URL
-    var url = new URL(request.url, 'http://localhost')
+  grunt.initConfig(config)
 
-    if (url.pathname !== '/encoding') {
-      next()
-      return
-    }
-
-    var cookieName = url.searchParams.get('name')
-    var cookieValue = url.searchParams.get('value')
-
-    response.setHeader('content-type', 'application/json')
-    response.end(
-      JSON.stringify({
-        name: cookieName,
-        value: cookieValue
-      })
-    )
-  }
-
-  grunt.initConfig({
-    qunit: {
-      all: {
-        options: {
-          urls: [
-            'http://127.0.0.1:9998/',
-            'http://127.0.0.1:9998/module.html',
-            'http://127.0.0.1:9998/encoding.html?integration_baseurl=http://127.0.0.1:9998/'
-          ]
-        }
-      }
-    },
-    nodeunit: {
-      all: 'test/node.js'
-    },
-    watch: {
-      options: {
-        livereload: true
-      },
-      files: ['src/**/*.mjs', 'test/**/*.js'],
-      tasks: 'default'
-    },
-    compare_size: {
-      files: [
-        'dist/js.cookie.min.mjs',
-        'dist/js.cookie.min.js',
-        'src/js.cookie.mjs'
-      ],
-      options: {
-        compress: {
-          gz: function (fileContents) {
-            return require('gzip-js').zip(fileContents, {}).length
-          }
-        }
-      }
-    },
-    connect: {
-      'build-qunit': {
-        options: {
-          port: 9998,
-          base: ['.', 'test'],
-          middleware: function (connect, options, middlewares) {
-            middlewares.unshift(encodingMiddleware)
-            return middlewares
-          }
-        }
-      },
-      tests: {
-        options: {
-          port: 10000,
-          base: ['.', 'test'],
-          open: 'http://127.0.0.1:10000',
-          keepalive: true,
-          livereload: true,
-          middleware: function (connect, options, middlewares) {
-            middlewares.unshift(encodingMiddleware)
-            return middlewares
-          }
-        }
-      }
-    },
-    exec: {
-      rollup: './node_modules/.bin/rollup -c',
-      lint: './node_modules/.bin/standard',
-      format:
-        './node_modules/.bin/prettier -l --write --single-quote --no-semi "**/*.{html,js,json,md,mjs}" && ./node_modules/.bin/eslint "**/*.{html,md}" --fix && ./node_modules/.bin/standard --fix',
-      'browserstack-runner': 'node_modules/.bin/browserstack-runner --verbose'
-    }
-  })
-
-  // Loading dependencies
-  for (var key in grunt.file.readJSON('package.json').devDependencies) {
-    if (key !== 'grunt' && key.indexOf('grunt') === 0) {
-      grunt.loadNpmTasks(key)
-    }
-  }
+  // Load dependencies
+  Object.keys(grunt.file.readJSON('package.json').devDependencies)
+    .filter(key => key !== 'grunt' && key.startsWith('grunt'))
+    .forEach(grunt.loadNpmTasks)
 
   grunt.registerTask('test', [
     'exec:lint',
