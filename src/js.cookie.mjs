b62eced0a55299a1c67daf87853e8533facd4c27
Nicolas Reynis <nicolas.reynis@actemedia.com>
2019-12-05 09:57:53 +0100

    Drop string casting

--- src/js.cookie.mjs
+++ src/js.cookie.mjs
@@ -30,12 +30,12 @@ function init (converter) {
 
     value = converter.write
       ? converter.write(value, key)
-      : encodeURIComponent(String(value)).replace(
+      : encodeURIComponent(value).replace(
         /%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g,
         decodeURIComponent
       )
 
-    key = encodeURIComponent(String(key))
+    key = encodeURIComponent(key)
       .replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent)
       .replace(/[()]/g, escape)
 
