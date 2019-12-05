9834ceb0de76d29629d6c69c4611587326e4043d
Klaus Hartl <klaus.hartl@gmail.com>
2019-12-05 14:58:20 +0100

    Replace snakecase naming

--- test/node.js
+++ test/node.js
@@ -1,24 +1,24 @@
 exports.node = {
-  should_load_js_cookie: function (test) {
+  shouldLoadApi: function (test) {
     test.expect(1)
     var Cookies = require('../dist/js.cookie.min.js')
     test.ok(!!Cookies.get, 'should load the Cookies API')
     test.done()
   },
-  should_not_throw_error_for_set_call_in_node: function (test) {
+  shouldNotThrowErrorForSetCallInNode: function (test) {
     test.expect(0)
     var Cookies = require('../dist/js.cookie.min.js')
     Cookies.set('anything')
     Cookies.set('anything', { path: '' })
     test.done()
   },
-  should_not_throw_error_for_get_call_in_node: function (test) {
+  shouldNotThrowErrorForGetCallInNode: function (test) {
     test.expect(0)
     var Cookies = require('../dist/js.cookie.min.js')
     Cookies.get('anything')
     test.done()
   },
-  should_not_throw_error_for_remove_call_in_node: function (test) {
+  shouldNotThrowErrorForRemoveCallInNode: function (test) {
     test.expect(0)
     var Cookies = require('../dist/js.cookie.min.js')
     Cookies.remove('anything')
