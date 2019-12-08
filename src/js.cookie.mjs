a3706c5ddb599d4c7f7dce6a5b8c0ac2a9476a9e
Klaus Hartl <klaus.hartl@gmail.com>
2019-12-08 15:41:23 +0100

    Remove unused argument

--- src/js.cookie.mjs
+++ src/js.cookie.mjs
@@ -10,10 +10,10 @@ function extend () {
 }
 
 var rfc6265Converter = {
-  read: function (value, key) {
+  read: function (value) {
     return value.replace(/(%[\dA-F]{2})+/gi, decodeURIComponent)
   },
-  write: function (value, key) {
+  write: function (value) {
     return encodeURIComponent(value).replace(
       /%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g,
       decodeURIComponent
