235d13f8f3a1491cc6f043cea504691d1a965375
Klaus Hartl <klaus.hartl@gmail.com>
2019-12-12 09:03:32 +0100

    Condense group in regex further

--- src/rfc6265.mjs
+++ src/rfc6265.mjs
@@ -4,7 +4,7 @@ export default {
   },
   write: function (value) {
     return encodeURIComponent(value).replace(
-      /%(2[346BF]|3[ACDEF]|40|5[BDE]|60|7[BCD])/g,
+      /%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,
       decodeURIComponent
     )
   }
