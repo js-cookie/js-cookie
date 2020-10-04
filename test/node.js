5ce29d266c6b20b59fa7a4a9bedf353ece17488c
Rouven Weßling <me@rouvenwessling.de>
2020-10-04 18:33:54 +0200

    Generate CommonJS and ESM versions of JSCookie along with the UMD version.

--- test/node.js
+++ test/node.js
@@ -1,26 +1,26 @@
 exports.node = {
   shouldLoadApi: function (test) {
     test.expect(1)
-    var Cookies = require('../dist/js.cookie.min.js')
+    var Cookies = require('../dist/js.cookie.js')
     test.ok(!!Cookies.get, 'should load the Cookies API')
     test.done()
   },
   shouldNotThrowErrorForSetCallInNode: function (test) {
     test.expect(0)
-    var Cookies = require('../dist/js.cookie.min.js')
+    var Cookies = require('../dist/js.cookie.js')
     Cookies.set('anything')
     Cookies.set('anything', { path: '' })
     test.done()
   },
   shouldNotThrowErrorForGetCallInNode: function (test) {
     test.expect(0)
-    var Cookies = require('../dist/js.cookie.min.js')
+    var Cookies = require('../dist/js.cookie.js')
     Cookies.get('anything')
     test.done()
   },
   shouldNotThrowErrorForRemoveCallInNode: function (test) {
     test.expect(0)
-    var Cookies = require('../dist/js.cookie.min.js')
+    var Cookies = require('../dist/js.cookie.js')
     Cookies.remove('anything')
     Cookies.remove('anything', { path: '' })
     test.done()
