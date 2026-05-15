eb3c40e89731e99b8970faaf35ddad249c6c0020
Klaus Hartl <klaus.hartl@gmail.com>
2026-05-15 11:23:44 +0200

    Prevent cookie attribute injection
    Given that we are using a `for ... in` loop for assembling a cookie's
    attributes required for writing/removing, we are vulnerable to prototype
    pollution, where an attacker might attempt to add/overwrite certain
    attributes and with that broadening access or wiping out a cookie
    altogether.
    
    Such malicious attributes input could most likely come from an object
    parsed from a JSON string; for example looking like
    '{"__proto__":{"samesite":"None"}}'.
    
    Note that at the moment we're tied to using this kind of for-loop for
    compatibility with IE 10 + 11.

--- src/assign.mjs
+++ src/assign.mjs
@@ -2,6 +2,7 @@ export default function (target) {
   for (var i = 1; i < arguments.length; i++) {
     var source = arguments[i]
     for (var key in source) {
+      if (key === '__proto__') continue
       target[key] = source[key]
     }
   }
